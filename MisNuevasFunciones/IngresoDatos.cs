515428871ee573c96908e9390029d5fc6c2b81ed
ciminoce <ciminoce@yahoo.com.ar>
2022-05-03 09:21:54 -0300

    Agregar archivos de proyecto.

---
+++ MisNuevasFunciones/IngresoDatos.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisNuevasFunciones
+{
+    public class IngresoDatos
+    {
+        public static int PedirIntEnRango(string m, int min, int max)
+        {
+            int rv = PedirInt($"{m} (Entre {min} y {max}): ");
+
+            while (!(rv >= min && rv <= max))
+            {
+                Console.WriteLine("Valor fuera de rango");
+                rv = PedirInt($"{m} (Entre {min} y {max}): ");
+            }
+
+            return rv;
+        }
+
+        public static double PedirDouble(string m)
+        {
+            double rv;
+            string strRv;
+
+            strRv = PedirString(m);
+
+            while (!double.TryParse(strRv, out rv))
+            {
+                strRv = PedirString(m);
+            }
+
+            return rv;
+        }
+
+
+        public static int PedirInt(string m)
+        {
+            int rv;
+            string strRv;
+
+            strRv = PedirString(m);
+
+            while (!int.TryParse(strRv, out rv))
+            {
+                strRv = PedirString(m);
+            }
+
+            return rv;
+        }
+        public static string PedirString(string m)
+        {
+            string strRv = string.Empty;
+            while (true)
+            {
+
+                Console.Write(m);
+                strRv = Console.ReadLine();
+                if (strRv == string.Empty)
+                {
+                    Console.WriteLine("ERROR!!! El dato es requerido");
+
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            return strRv;
+        }
+
+    }
+}
---
+++ MisNuevasFunciones/Mostrar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MisNuevasFunciones.IngresoDatos;
+
+namespace MisNuevasFunciones
+{
+    public class Mostrar
+    {
+        public static int MostrarMenu(string[] opciones)
+        {
+            for (int i = 0; i < opciones.Length; i++)
+            {
+                Console.WriteLine($"{opciones[i]}");
+            }
+
+            return PedirIntEnRango("Su eleccion:", 1, opciones.Length);
+        }
+
+    }
+}
